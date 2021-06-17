a3922c6df5fda59d581d2e4c2676ca34212c66a8
Marketing 1 <Marketing 1@MARKETING1>
2021-06-17 16:05:13 +0300

    ref

--- sources/ENGINE/Object.h
+++ sources/ENGINE/Object.h
@@ -7,20 +7,18 @@
 #include "ICollidable.h"
 
 // refs
-class	ENGINE_API	IRender_Visual;
-class	ENGINE_API	IRender_Sector;
-class	ENGINE_API	IRender_ObjectSpecific;
-class	ENGINE_API	CCustomHUD;
-class	CNetPacket	;
-class	CSE_Abstract;
+class ENGINE_API IRender_Visual;
+class ENGINE_API IRender_Sector;
+class ENGINE_API IRender_ObjectSpecific;
+class ENGINE_API CCustomHUD;
+class CNetPacket;
+class CSE_Abstract;
 
-//-----------------------------------------------------------------------------------------------------------
-#define CROW_RADIUS	(30.f)
-//-----------------------------------------------------------------------------------------------------------
-//	CObject
-//-----------------------------------------------------------------------------------------------------------
-#pragma pack(push,4)
-class	ENGINE_API						CObject :	
+
+#define CROW_RADIUS													30.0f
+
+#pragma pack(push, 4)
+class ENGINE_API CObject :
 	public DLL_Pure,
 	public ISpatial,
 	public ISheduled,
@@ -28,164 +26,322 @@ class	ENGINE_API						CObject :
 	public ICollidable
 {
 public:
-	struct	SavedPosition
+	struct SavedPosition
 	{
-		u32			dwTime;
-		Fvector3		vPosition;
+		u32																dwTime;
+		Fvector3														vPosition;
 	};
-	union	ObjectProperties
-	{
-		struct 
-		{
-			u32	net_ID			:	16;
-			u32	bActiveCounter	:	8;
-			u32	bEnabled		:	1;
-			u32	bVisible		:	1;
-			u32	bDestroy		:	1;
-			u32	net_Local		:	1;
-			u32	net_Ready		:	1;
-			u32 net_SV_Update	:	1;
-			u32 crow			:	1;
-			u32	bPreDestroy		:	1;
-		};
-		u32	storage;
-	};
-private:
-	// Some property variables
-	ObjectProperties					Props;
-	shared_str							NameObject;
-	shared_str							NameSection;
-	shared_str							NameVisual;
-protected:
-	// Parentness
-	CObject*							Parent;
 
-	// Geometric (transformation)
-	svector<SavedPosition,4>			PositionStack;
-public:
+	union ObjectProperties
+	{
+		struct
+		{
+			u32																net_ID			: 16;
+			u32																bActiveCounter	: 8;
+			u32																bEnabled		: 1;
+			u32																bVisible		: 1;
+			u32																bDestroy		: 1;
+			u32																net_Local		: 1;
+			u32																net_Ready		: 1;
+			u32																net_SV_Update	: 1;
+			u32																crow			: 1;
+			u32																bPreDestroy		: 1;
+		};
+
+		u32																storage;
+	};
+
 #ifdef DEBUG
-	u32									dbg_update_cl;
-#endif
-	u32									dwFrame_UpdateCL;
-	u32									dwFrame_AsCrow;
+	u32																dbg_update_cl;
+#endif // def DEBUG
+
+	u32																dwFrame_UpdateCL;
+	u32																dwFrame_AsCrow;
 
 	// Crow-MODE
 	// if (object_is_visible)
 	// if (object_is_near)
 	// if (object_is_crow_always)
-		void							MakeMeCrow_internal	();
-		void							MakeMeCrow			();
+	void								MakeMeCrow_internal			( );
+	void								MakeMeCrow					( );
 
-		__forceinline	void							IAmNotACrowAnyMore	()					{ Props.crow = false;	}
-	virtual BOOL						AlwaysTheCrow		()					{ return FALSE; }
+	__forceinline void					IAmNotACrowAnyMore			( )
+	{
+		Props.crow = false;
+	}
+	virtual BOOL						AlwaysTheCrow				( )
+	{
+		return FALSE;
+	}
 
 	// Network
-	__forceinline BOOL							Local				()			const	{ return Props.net_Local;	}
-	__forceinline BOOL							Remote				()			const	{ return !Props.net_Local;	}
-	__forceinline U16								ID					()			const	{ return Props.net_ID;		}
-	__forceinline void							setID				(U16 _ID)			{ Props.net_ID = _ID;		}
-	virtual BOOL						Ready				()					{ return Props.net_Ready;	}
-	BOOL								GetTmpPreDestroy		()		const	{ return Props.bPreDestroy;	}
-	void								SetTmpPreDestroy	(BOOL b)			{ Props.bPreDestroy = b;}
-	virtual float						shedule_Scale		()					{ return Device.vCameraPosition.distance_to(Position())/200.f; }
-	virtual bool						shedule_Needed		()					{return processing_enabled();};
+	__forceinline BOOL					Local						( ) const
+	{
+		return Props.net_Local;
+	}
+	__forceinline BOOL					Remote						( ) const
+	{
+		return !Props.net_Local;
+	}
+	__forceinline U16					ID							( ) const
+	{
+		return Props.net_ID;
+	}
+	__forceinline void					setID						(U16 _ID)
+	{
+		Props.net_ID = _ID;
+	}
+	virtual BOOL						Ready						( )
+	{
+		return Props.net_Ready;
+	}
+	BOOL								GetTmpPreDestroy			( ) const
+	{
+		return Props.bPreDestroy;
+	}
+	void								SetTmpPreDestroy			(BOOL b)
+	{
+		Props.bPreDestroy = b;
+	}
+	virtual float						shedule_Scale				( )
+	{
+		return Device.vCameraPosition.distance_to(Position( )) / 200.0f;
+	}
+	virtual bool						shedule_Needed				( )
+	{
+		return processing_enabled( );
+	}
 
 	// Parentness
-	inline CObject*							H_Parent			()					{ return Parent;						}
-	inline const CObject*					H_Parent			()			const	{ return Parent;						}
-	CObject*							H_Root				()					{ return Parent?Parent->H_Root():this;	}
-	const CObject*						H_Root				()			const	{ return Parent?Parent->H_Root():this;	}
-	CObject*							H_SetParent			(CObject* O, bool just_before_destroy = false);
+	inline CObject*						H_Parent					( )
+	{
+		return Parent;
+	}
+	inline const CObject*				H_Parent					( ) const
+	{
+		return Parent;
+	}
+	CObject*							H_Root						( )
+	{
+		return Parent ? Parent->H_Root( ) : this;
+	}
+	const CObject*						H_Root						( ) const
+	{
+		return Parent ? Parent->H_Root( ) : this;
+	}
+	CObject*							H_SetParent					(CObject* O, bool just_before_destroy = false);
 
 	// Geometry xform
-	virtual void						Center				(Fvector3& C) const;
-	inline const Fmatrix&					XFORM				()			 const	{ VERIFY(_valid(renderable.xform));	return renderable.xform;	}
-	__forceinline Fmatrix&						XFORM				()					{ return renderable.xform;			}
-	virtual void						spatial_register	();
-	virtual void						spatial_unregister	();
-	virtual void						spatial_move		();
-	void								spatial_update		(float eps_P, float eps_R);
+	virtual void						Center						(Fvector3& C) const;
+	inline const Fmatrix&				XFORM						( ) const
+	{
+		VERIFY(_valid(renderable.xform));
+		return renderable.xform;
+	}
+	__forceinline Fmatrix&				XFORM						( )
+	{
+		return renderable.xform;
+	}
+	virtual void						spatial_register			( );
+	virtual void						spatial_unregister			( );
+	virtual void						spatial_move				( );
+	void								spatial_update				(float eps_P, float eps_R);
 
-	__forceinline Fvector3&						Direction			() 					{ return renderable.xform.k;		}
-	__forceinline const Fvector3&					Direction			() 			const	{ return renderable.xform.k;		}
-	__forceinline Fvector3&						Position			() 					{ return renderable.xform.c;		}
-	__forceinline const Fvector3&					Position			() 			const	{ return renderable.xform.c;		}
-	virtual float						Radius				()			const;
-	virtual const Fbox3&					BoundingBox			()			const;
-	
-	inline IRender_Sector*					Sector				()					{ return H_Root()->spatial.sector;	}
-	inline IRender_ObjectSpecific*			ROS					()					{ return renderable_ROS();			}
-	virtual BOOL						renderable_ShadowGenerate	()			{ return TRUE;						}
-	virtual BOOL						renderable_ShadowReceive	()			{ return TRUE;						}
+	__forceinline Fvector3&				Direction					( )
+	{
+		return renderable.xform.k;
+	}
+	__forceinline const Fvector3&		Direction					( ) const
+	{
+		return renderable.xform.k;
+	}
+	__forceinline Fvector3&				Position					( )
+	{
+		return renderable.xform.c;
+	}
+	__forceinline const Fvector3&		Position					( ) const
+	{
+		return renderable.xform.c;
+	}
+	virtual float						Radius						( ) const;
+	virtual const Fbox3&				BoundingBox					( ) const;
+
+	inline IRender_Sector*				Sector						( )
+	{
+		return H_Root( )->spatial.sector;
+	}
+	inline IRender_ObjectSpecific*		ROS							( )
+	{
+		return renderable_ROS( );
+	}
+	virtual BOOL						renderable_ShadowGenerate	( )
+	{
+		return TRUE;
+	}
+	virtual BOOL						renderable_ShadowReceive	( )
+	{
+		return TRUE;
+	}
 
 	// Accessors and converters
-	__forceinline IRender_Visual*					Visual				()					{ return renderable.visual;			}
-	__forceinline ICollisionForm*					CFORM				() const			{ return collidable.model;			}
-	virtual		CObject*				dcast_CObject		()					{ return this;						}
-	virtual		IRenderable*			dcast_Renderable	()					{ return this;						}
-	virtual void						OnChangeVisual		()					{ }
+	__forceinline IRender_Visual*		Visual						( )
+	{
+		return renderable.visual;
+	}
+	__forceinline ICollisionForm*		CFORM						( ) const
+	{
+		return collidable.model;
+	}
+	virtual CObject*					dcast_CObject				( )
+	{
+		return this;
+	}
+	virtual IRenderable*				dcast_Renderable			( )
+	{
+		return this;
+	}
+	virtual void						OnChangeVisual				( )
+	{ }
 
 	// Name management
-	__forceinline shared_str						cName				()			const	{ return NameObject;				}
-	void								cName_set			(shared_str N);
-	__forceinline shared_str						cNameSect			()			const	{ return NameSection;				}
-	void								cNameSect_set		(shared_str N);
-	__forceinline shared_str						cNameVisual			()			const	{ return NameVisual;				}
-	void								cNameVisual_set		(shared_str N);
-	virtual	shared_str					shedule_Name		() const			{ return cName(); };
-	
-	// Properties
-	void								processing_activate		();				// request	to enable	UpdateCL
-	void								processing_deactivate	();				// request	to disable	UpdateCL
-	bool								processing_enabled		()				{ return 0!=Props.bActiveCounter;	}
+	__forceinline shared_str			cName						( ) const
+	{
+		return NameObject;
+	}
+	void								cName_set					(shared_str N);
+	__forceinline shared_str			cNameSect					( ) const
+	{
+		return NameSection;
+	}
+	void								cNameSect_set				(shared_str N);
+	__forceinline shared_str			cNameVisual					( ) const
+	{
+		return NameVisual;
+	}
+	void								cNameVisual_set				(shared_str N);
+	virtual shared_str					shedule_Name				( ) const
+	{
+		return cName( );
+	}
 
-	void								setVisible			(BOOL _visible);
-	__forceinline BOOL							getVisible			()			const	{ return Props.bVisible;			}
-	void								setEnabled			(BOOL _enabled);
-	__forceinline BOOL							getEnabled			()			const	{ return Props.bEnabled;			}
-		void							setDestroy			(BOOL _destroy);
-		__forceinline BOOL							getDestroy			()			const	{ return Props.bDestroy;			}
-		__forceinline void							setLocal			(BOOL _local)		{ Props.net_Local = _local?1:0;		}
-		__forceinline BOOL							getLocal			()			const	{ return Props.net_Local;			}
-		__forceinline void							setSVU				(BOOL _svu)			{ Props.net_SV_Update	= _svu?1:0;	}
-		__forceinline BOOL							getSVU				()			const	{ return Props.net_SV_Update;		}
-		__forceinline void							setReady			(BOOL _ready)		{ Props.net_Ready = _ready?1:0;		}
-		__forceinline BOOL							getReady			()			const	{ return Props.net_Ready;			}
+	// Properties
+	// request to enable UpdateCL
+	void								processing_activate			( );
+	// request to disable UpdateCL
+	void								processing_deactivate		( );
+	bool								processing_enabled			( )
+	{
+		return 0 != Props.bActiveCounter;
+	}
+
+	void								setVisible					(BOOL _visible);
+	__forceinline BOOL					getVisible					( ) const
+	{
+		return Props.bVisible;
+	}
+	void								setEnabled					(BOOL _enabled);
+	__forceinline BOOL					getEnabled					( ) const
+	{
+		return Props.bEnabled;
+	}
+	void								setDestroy					(BOOL _destroy);
+	__forceinline BOOL					getDestroy					( ) const
+	{
+		return Props.bDestroy;
+	}
+	__forceinline void					setLocal					(BOOL _local)
+	{
+		Props.net_Local = _local ? 1 : 0;
+	}
+	__forceinline BOOL					getLocal					( ) const
+	{
+		return Props.net_Local;
+	}
+	__forceinline void					setSVU						(BOOL _svu)
+	{
+		Props.net_SV_Update = _svu ? 1 : 0;
+	}
+	__forceinline BOOL					getSVU						( ) const
+	{
+		return Props.net_SV_Update;
+	}
+	__forceinline void					setReady					(BOOL _ready)
+	{
+		Props.net_Ready = _ready ? 1 : 0;
+	}
+	__forceinline BOOL					getReady					( ) const
+	{
+		return Props.net_Ready;
+	}
 
 	//---------------------------------------------------------------------
-										CObject				();
-	virtual								~CObject			();
+										CObject						( );
+	virtual								~CObject					( );
 
-	virtual void						Load				(const char* section);
-	
+	virtual void						Load						(const char* section);
+
 	// Update
-	virtual void						shedule_Update		(u32 dt);							// Called by sheduler
-	virtual void						renderable_Render	();
+	// Called by sheduler
+	virtual void						shedule_Update				(u32 dt);
+	virtual void						renderable_Render			( );
 
-	virtual void						UpdateCL			();									// Called each frame, so no need for dt
-	virtual BOOL						net_Spawn			(CSE_Abstract* data);
-	virtual void						net_Destroy			();
-	virtual void						net_Export			(CNetPacket& P) {};					// export to server
-	virtual void						net_Import			(CNetPacket& P) {};					// import from server
-	virtual	void						net_ImportInput		(CNetPacket& P)	{};
-	virtual BOOL						net_Relevant		()				{ return FALSE; };	// relevant for export to server
-	virtual void						net_Relcase			(CObject*	 O) { };				// destroy all links to another objects
+	// Called each frame, so no need for dt
+	virtual void						UpdateCL					( );
+	virtual BOOL						net_Spawn					(CSE_Abstract* data);
+	virtual void						net_Destroy					( );
+	// export to server
+	virtual void						net_Export					(CNetPacket& P)
+	{ }
+	// import from server
+	virtual void						net_Import					(CNetPacket& P)
+	{ }
+	virtual void						net_ImportInput				(CNetPacket& P)
+	{ }
+	// relevant for export to server
+	virtual BOOL						net_Relevant				( )
+	{
+		return FALSE;
+	}
+	// destroy all links to another objects
+	virtual void						net_Relcase					(CObject* O)
+	{ }
 
 	// Position stack
-	inline u32								ps_Size				()			const	{ return PositionStack.size(); }
-	virtual	SavedPosition				ps_Element			(u32 ID)	const;
-	virtual void						ForceTransform		(const Fmatrix& m)	{};
+	inline u32							ps_Size						( ) const
+	{
+		return PositionStack.size( );
+	}
+	virtual SavedPosition				ps_Element					(u32 ID) const;
+	virtual void						ForceTransform				(const Fmatrix& m)
+	{ }
 
 	// HUD
-	virtual void						OnHUDDraw			(CCustomHUD* hud)	{};
+	virtual void						OnHUDDraw					(CCustomHUD* hud)
+	{ }
 
 	// Active/non active
-	virtual void						OnH_B_Chield		();		// before
-	virtual void						OnH_B_Independent	(bool just_before_destroy);
-	virtual void						OnH_A_Chield		();		// after
-	virtual void						OnH_A_Independent	();
+	virtual void						OnH_B_Chield				( );		// before
+	virtual void						OnH_B_Independent			(bool just_before_destroy);
+	virtual void						OnH_A_Chield				( );		// after
+	virtual void						OnH_A_Independent			( );
 
-public:
-	virtual bool						register_schedule	() const {return true;}
+	virtual bool						register_schedule			( ) const
+	{
+		return true;
+	}
+
+private:
+	// Some property variables
+	ObjectProperties												Props;
+	shared_str														NameObject;
+	shared_str														NameSection;
+	shared_str														NameVisual;
+
+protected:
+	// Parentness
+	CObject*														Parent;
+
+	// Geometric (transformation)
+	svector<SavedPosition, 4>										PositionStack;
 };
 #pragma pack(pop)
--- sources/ENGINE/ObjectList.h
+++ sources/ENGINE/ObjectList.h
@@ -1,81 +1,69 @@
 #pragma once
 
 // refs
-class ENGINE_API	CObject;
+class ENGINE_API CObject;
 class CNetPacket;
 
-class ENGINE_API 				CObjectList
+class ENGINE_API CObjectList
 {
-private:
-	// data
-	xr_map<u32, CObject*>		map_NETID;
-	xr_vector<CObject*>			destroy_queue;
-	xr_vector<CObject*>			objects_active;
-	xr_vector<CObject*>			objects_sleeping;
-
-	xr_vector<CObject*>			crows_0;
-	xr_vector<CObject*>			crows_1;
-	xr_vector<CObject*>* crows;
-
-	CObject** objects_dup;
-	u32							objects_dup_memsz;
-
 public:
-	typedef fastdelegate::FastDelegate1<CObject*>	RELCASE_CALLBACK;
+	using RELCASE_CALLBACK											= fastdelegate::FastDelegate1<CObject*>;
+
 	struct SRelcasePair
 	{
 		int* m_ID;
-		RELCASE_CALLBACK		m_Callback;
-		SRelcasePair(int* id, RELCASE_CALLBACK cb) :m_ID(id), m_Callback(cb)
+		RELCASE_CALLBACK												m_Callback;
+										SRelcasePair					(int* id, RELCASE_CALLBACK cb) : m_ID(id), m_Callback(cb)
 		{ }
-		bool	operator==			(RELCASE_CALLBACK cb)
+		bool							operator ==						(RELCASE_CALLBACK cb)
 		{
 			return m_Callback == cb;
 		}
 	};
-	typedef xr_vector<SRelcasePair>					RELCASE_CALLBACK_VEC;
-	RELCASE_CALLBACK_VEC							m_relcase_callbacks;
 
-	void						relcase_register(RELCASE_CALLBACK, int*);
-	void						relcase_unregister(int*);
+	using RELCASE_CALLBACK_VEC										= xr_vector<SRelcasePair>;
+	RELCASE_CALLBACK_VEC											m_relcase_callbacks;
+
+	void							relcase_register				(RELCASE_CALLBACK, int*);
+	void							relcase_unregister				(int*);
 
 	// methods
-	CObjectList( );
-	~CObjectList( );
+									CObjectList						( );
+									~CObjectList					( );
 
-	CObject* FindObjectByName(shared_str	name);
-	CObject* FindObjectByName(const char* name);
-	CObject* FindObjectByCLS_ID(CLASS_ID		cls);
+	CObject*						FindObjectByName				(shared_str name);
+	CObject*						FindObjectByName				(const char* name);
+	CObject*						FindObjectByCLS_ID				(CLASS_ID cls);
 
-	void						Load( );
-	void						Unload( );
+	void							Load							( );
+	void							Unload							( );
 
-	CObject* Create(const char* name);
-	void						Destroy(CObject* O);
+	CObject*						Create							(const char* name);
+	void							Destroy							(CObject* O);
 
-	void						SingleUpdate(CObject* O);
-	void						Update(bool bForce);
+	void							SingleUpdate					(CObject* O);
+	void							Update							(bool bForce);
 
-	void						net_Register(CObject* O);
-	void						net_Unregister(CObject* O);
+	void							net_Register					(CObject* O);
+	void							net_Unregister					(CObject* O);
 
-	u32							net_Export(CNetPacket* P, u32 _start, u32 _count);	// return next start
-	void						net_Import(CNetPacket* P);
-	CObject* net_Find(u32 ID);
+	u32								net_Export						(CNetPacket* P, u32 _start, u32 _count);	// return next start
+	void							net_Import						(CNetPacket* P);
+	CObject*						net_Find						(u32 ID);
 
-	void						o_crow(CObject* O)
+	void							o_crow							(CObject* O)
 	{
 		crows->push_back(O);
 	}
 
-	void						o_remove(xr_vector<CObject*>& v, CObject* O);
-	void						o_activate(CObject* O);
-	void						o_sleep(CObject* O);
-	inline u32						o_count( )
+	void							o_remove						(xr_vector<CObject*>& v, CObject* O);
+	void							o_activate						(CObject* O);
+	void							o_sleep							(CObject* O);
+	inline u32						o_count							( )
 	{
 		return objects_active.size( ) + objects_sleeping.size( );
-	};
-	inline CObject* o_get_by_iterator(u32 _it)
+	}
+	inline CObject*					o_get_by_iterator				(u32 _it)
 	{
 		if (_it < objects_active.size( ))
 		{
@@ -86,12 +74,25 @@ public:
 			return objects_sleeping[_it - objects_active.size( )];
 		}
 	}
-	bool						dump_all_objects( );
+	bool							dump_all_objects				( );
 
-	void				register_object_to_destroy(CObject* object_to_destroy);
+	void							register_object_to_destroy		(CObject* object_to_destroy);
 
 #ifdef DEBUG
-	bool				registered_object_to_destroy(const CObject* object_to_destroy) const;
+	bool							registered_object_to_destroy	(const CObject* object_to_destroy) const;
 #endif // def DEBUG
 
+private:
+	// data
+	xr_map<u32, CObject*>											map_NETID;
+	xr_vector<CObject*>												destroy_queue;
+	xr_vector<CObject*>												objects_active;
+	xr_vector<CObject*>												objects_sleeping;
+
+	xr_vector<CObject*>												crows_0;
+	xr_vector<CObject*>												crows_1;
+	xr_vector<CObject*>*											crows;
+
+	CObject**														objects_dup;
+	u32																objects_dup_memsz;
 };
