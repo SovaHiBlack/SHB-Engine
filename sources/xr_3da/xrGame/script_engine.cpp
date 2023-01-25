f3f6289a72069407fb9c1f3106859d44440a2787
SovaHiBlack <40763374+SovaHiBlack@users.noreply.github.com>
2023-01-25 07:24:46 +0300

    просев вывода в log

--- sources/xr_3da/xrGame/script_engine.cpp
+++ sources/xr_3da/xrGame/script_engine.cpp
@@ -254,9 +254,11 @@ void CScriptEngine::process_file_if_exists	(LPCSTR file_name, bool warn_if_not_e
 			add_no_file		(file_name,string_length);
 			return;
 		}
-#ifndef MASTER_GOLD
-		Msg					("* loading script %s",S1);
-#endif // MASTER_GOLD
+
+#ifdef LOADING_SCRIPT_LOG
+		Msg					("* loading script: [%s]",S1);
+#endif // def LOADING_SCRIPT_LOG
+
 		m_reload_modules	= false;
 		load_file_into_namespace(S,*file_name ? file_name : "_G");
 	}
